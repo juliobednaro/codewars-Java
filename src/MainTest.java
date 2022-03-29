8a9fe7cb94a221d520e8cdd4c9091fde4463d7ba
polskiprogramator <33041529+polskiprogramator@users.noreply.github.com>
2022-03-29 18:23:13 +0200

    solution

--- src/MainTest.java
+++ src/MainTest.java
@@ -1,13 +1,10 @@
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
-import one.Kata;
+import two.Kata;
 
 public class MainTest {
     @Test
     public void testBasic() {
-        assertEquals(9, Kata.squareSum(new int[] {1,2,2}));
-        assertEquals(5, Kata.squareSum(new int[] {1,2}));
-        assertEquals(50, Kata.squareSum(new int[] {5,-3,4}));
-        assertEquals(0, Kata.squareSum(new int[] {}));
+        assertEquals("Hello, Ryan how are you doing today?", Kata.greet("Ryan"));
     }
 }
